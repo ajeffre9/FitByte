908975e7231ec7d1e9cad46614bdc4d1bd7620b2
ajeffre9 <ajeffre9@wo.ca>
2016-02-25 18:55:18 -0500

    Forgot to push the name

---
+++ FitByte/src/main/java/ca/uwo/csd/cs2212/team04/Settings.java
@@ -0,0 +1,17 @@
+package ca.uwo.csd.cs2212.team04;
+
+/**
+ * Created by owner on 2016-02-24.
+ */
+public class Settings {
+
+    private Object date, dashborad, colorTheme, dailyGoals, CustomGoals;
+
+    public void Strings(){
+        date = null;
+        dashborad = null;
+        colorTheme = null;
+        dailyGoals = null;
+
+    }
+}
--- FitByte/src/main/java/ca/uwo/csd/cs2212/team04/User.java
+++ FitByte/src/main/java/ca/uwo/csd/cs2212/team04/User.java
@@ -1,7 +1,107 @@
 package ca.uwo.csd.cs2212.team04;
 
+import com.sun.java.swing.plaf.windows.WindowsBorders;
+
+import java.util.Date;
+
 /**
  * Created by owner on 2016-02-17.
  */
 public class User {
+
+    //Attributes for settings class
+
+//    private Date date; //Not sure if the is is teh right one
+//    private WindowsBorders.DashedBorder dashborad;
+//    private ColorTheme colorTheme;
+//    private DailyGoals DailyGOals;
+//    private CustomGoals CustomGoals;
+
+    private String name, gender, hobby;
+    private double weight, height;
+    private int age;
+    private Object dailyGoals, lifeGoals, address;
+
+    public void User(){
+        name = null;
+        gender =null;
+        hobby = null;
+        weight = 0;
+        height  = 0;
+        age = 0;
+        dailyGoals = null;
+        lifeGoals = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Object getDailyGoals() {
+        return dailyGoals;
+    }
+
+    public void setDailyGoals(Object dailyGoals) {
+        this.dailyGoals = dailyGoals;
+    }
+
+    public Object getLifeGoals() {
+        return lifeGoals;
+    }
+
+    public void setLifeGoals(Object lifeGoals) {
+        this.lifeGoals = lifeGoals;
+    }
+
+    public Object getAddress() {
+        return address;
+    }
+
+    public void setAddress(Object address) {
+        this.address = address;
+    }
 }
