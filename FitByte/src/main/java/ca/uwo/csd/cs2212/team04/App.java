233cb7fe60d428d2be135de6cb3fd5e689fd3873
Duncan Iglesias <diglesia@uwo.ca>
2016-03-01 18:52:09 -0500

    Adding App.java to API branch

--- FitByte/src/main/java/ca/uwo/csd/cs2212/team04/App.java
+++ FitByte/src/main/java/ca/uwo/csd/cs2212/team04/App.java
@@ -6,6 +6,19 @@ import java.io.*;
 public class App implements Serializable {
 
 	public static void main(String [] args) throws Exception {
+		
+
+		// read in whether this is a test mode
+		if (args.length >= 1) {
+			if (args[0].equals("test")) {
+				System.err.println("This is a test");
+			} 
+		} else {
+			System.err.println("This is not a test");
+		}
+
+
+
 		// begin the system
 		Settings settings;
 		User user;
@@ -22,20 +35,27 @@ public class App implements Serializable {
 	      	System.err.println("Settings successfully read");
 
 	    } catch (Exception e) {
-	        System.err.println(e + ": loadSettings failed");
-
+	        //System.err.println(e + ": loadSettings failed");
+			System.err.println("New  settings file created");
 	        // create a blank settings file
 	        settings = new Settings();
 	        settings.saveSettings();
 	    }
 
-		// open the interface (GUI)
-
-
-
-
-
+		
 		// request data from fitbit sever
+
+
+
+		// open the interface (GUI)
+	    GUI gui = new GUI(settings, user);
+
+	    
+
+
+
+
+		/
 
 		// 
 
